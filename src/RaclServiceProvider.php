8c9c71a9f6d62ba41ba3d5bd36733a00647eaec4
Jose Diaz <xoxefdp@gmail.com>
2022-02-19 14:23:57 +0100

    Update service provider for racl

--- src/RaclServiceProvider.php
+++ src/RaclServiceProvider.php
@@ -9,19 +9,54 @@ use Xiscodev\Racl\Requester\RaclRequester;
 class RaclServiceProvider extends ServiceProvider
 {
     /**
+     * The classes to be registered.
+     *
+     * @var array
+     */
+    protected $classes = [
+        'RaclResponser' => RaclResponser::class,
+        'RaclRequester' => RaclRequester::class,
+    ];
+
+    /**
+     * Register the given classes.
+     *
+     * @param array $classes
+     */
+    protected function registerClasses(array $classes)
+    {
+        foreach (array_keys($classes) as $class) {
+            $method = "register{$class}Class";
+
+            call_user_func_array([$this, $method], []);
+        }
+
+        $this->classes(array_values($classes));
+    }
+
+
+    public function registerRaclResponserClass()
+    {
+        $this->app->singleton($this->classes['RaclResponser'], function () {
+            return new RaclResponser();
+        });
+    }
+
+    public function registerRaclRequesterClass()
+    {
+        $this->app->singleton($this->classes['RaclRequester'], function () {
+            return new RaclRequester();
+        });
+    }
+
+    /**
      * Register any application services.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->singleton(RaclResponser::class, function ($app) {
-            return new RaclResponser();
-        });
-
-        $this->app->singleton(RaclRequester::class, function ($app) {
-            return new RaclRequester();
-        });
+        $this->registerClasses($this->classes);
     }
 
     /**
@@ -31,6 +66,6 @@ class RaclServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return [RaclResponser::class, RaclRequester::class];
+        return array_values($this->classes);
     }
 }
