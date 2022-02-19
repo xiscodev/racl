511a00c88552aae5866d2f11bcf27643cfdfb145
Jose Diaz <xoxefdp@gmail.com>
2022-02-19 13:50:46 +0100

    Added comments, added provides method

--- src/RaclServiceProvider.php
+++ src/RaclServiceProvider.php
@@ -8,14 +8,29 @@ use Xiscodev\Racl\Requester\RaclRequester;
 
 class RaclServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->app->singleton(RaclResponser::class, function () {
+        $this->app->singleton(RaclResponser::class, function ($app) {
             return new RaclResponser();
         });
 
-        $this->app->singleton(RaclRequester::class, function () {
+        $this->app->singleton(RaclRequester::class, function ($app) {
             return new RaclRequester();
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [RaclResponser::class, RaclRequester::class];
     }
 }
